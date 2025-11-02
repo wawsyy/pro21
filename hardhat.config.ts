ea5936c71f15bedc4f60636cff94d8b69eaf12a1
wawsyy <shiyu689@qq.com>
2025-11-01 16:05:10 -0800

    fix: increase optimizer runs for better gas optimization

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -79,7 +79,7 @@ const config: HardhatUserConfig = {
       // https://hardhat.org/hardhat-network/#solidity-optimizer-support
       optimizer: {
         enabled: true,
-        runs: 800,
+        runs: 1000,
       },
       evmVersion: "cancun",
     },
